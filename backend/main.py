3594987bd1d9c6da066663bb7a1794bfda824637
Khaoula Lakhdim <khaoulalakhdim@khaoulas.local>
2025-08-25 11:23:21 +0100

    a few addition to prep

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 app= FastAPI()
+
+origins= ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_method=["*"],
+    allow_headers=["*"],
+)
+
+
 
 @app.get("/")
 def welcome():
     return {"message":"welcome to cv refactor app"}
+
+
